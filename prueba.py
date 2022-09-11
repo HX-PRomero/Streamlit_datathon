15353fd08dbcc7c3737f5efde580e39822941b8e
pjr95 <pablo.romero@mi.unc.edu.ar>
2022-09-11 23:40:21 +0200

    Actualización

--- prueba.py
+++ prueba.py
@@ -2,5 +2,5 @@ import os
 from time import sleep
 
 while True:
-    os.system('cmd /k "git add . & git commit -m "Actualización" & git push origin main"')
-    sleep(900)
+    os.system('cmd /c "git add . & git commit -m "Actualización" & git push origin main"')
+    sleep(5)
