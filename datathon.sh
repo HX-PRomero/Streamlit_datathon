4b0875e37a6b1b2e43b3caf6bff6bf67ec4652e2
pjr95 <pablo.romero@mi.unc.edu.ar>
2022-09-11 23:37:57 +0200

    Actualización

--- datathon.sh
+++ datathon.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd /home/pablo/Documents/git-projects/henry/labs/Streamlit_datathon/
-git -C /home/pablo/Documents/git-projects/henry/labs/Streamlit_datathon/ add .
-git -C /home/pablo/Documents/git-projects/henry/labs/Streamlit_datathon/ commit -m "Actualización Datathon"
-git -C /home/pablo/Documents/git-projects/henry/labs/Streamlit_datathon/ push origin main
+git add .
+git commit -m "Actualización Datathon"
+git push origin main
---
+++ prueba.py
@@ -0,0 +1,2 @@
+import os
+os.system('cmd /k "git add . & git commit -m "Actualización" & git push origin main"')
