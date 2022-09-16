fc8569be5ab913a1c70c7b65ea037f9a953962bd
pjr95 <pablo.romero@mi.unc.edu.ar>
2022-09-16 15:50:11 +0200

    Modifications eval.py

--- eval.py
+++ eval.py
@@ -5,36 +5,49 @@ from time import sleep
 
 def metrica(solution, response, metrica):
     if metrica == 'ECM':
-        ecm = ((solution - response).sum()**2)/len(response)
+        ecm = ((solution - response)**2).sum()/len(response)
         return ecm
     if metrica == 'RECM':
-       recm = sqrt(((solution - response).sum()**2)/len(response))
+       recm = sqrt(((solution - response)**2).sum()/len(response))
        return recm
 
 use_metric = 'RECM'
 path_res = 'G:/My Drive/Datathon_responses/responses/'
 file_sol = 'G:/My Drive/Datathon_solutions/solution.csv'
 
-files = os.listdir(path_res)
+
 
 solution = pd.read_csv(file_sol)
-responses = {}
+students = pd.read_excel('Data 02 Alumnos.xlsx')
+
 
 while True:
+    files = os.listdir(path_res)
+    responses = {}
     for file in files:
         if '.csv' not in file:
+            print('Not in file')
             continue
         name = file.split(' ')
-        response = pd.read_csv(path_res + file)
         try:
-            responses[name[0]] = metrica(solution, response, use_metric)
+            response = pd.read_csv(path_res + file)
         except:
+            print('cant read')
+            continue
+        try:
+            recm = metrica(solution, response, use_metric)
+            if name[0] in list(responses.keys()):
+                   responses[name[0]+name[len(name)-1].replace('.csv','')] = recm
+            else:
+                responses[name[0]] = recm
+        except:
+            print("cant save")
             continue
     metric = pd.DataFrame(responses.values(), index = responses.keys())
 
     metric.columns = [use_metric]
 
-    metric.sort_values('', axis = 1)
+    metric.sort_values(by = use_metric, axis = 0, inplace=True)
 
-    metric.to_csv('G:/My Drive/Datathon_solutions/metrics.csv')
+    metric.to_excel('G:/My Drive/Datathon_solutions/metrics.xlsx')
     sleep(60)
