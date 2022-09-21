8262ce720d84ab4716eccff2b6e5d890cf88fe9c
pjr95 <pablo.romero@mi.unc.edu.ar>
2022-09-21 14:17:54 +0200

    eval.py changes

--- eval.py
+++ eval.py
@@ -8,17 +8,27 @@ def metrica(solution, response, metrica):
         ecm = ((solution - response)**2).sum()/len(response)
         return ecm
     if metrica == 'RECM':
-       recm = sqrt(((solution - response)**2).sum()/len(response))
-       return recm
+        recm = sqrt(((solution - response)**2).sum()/len(response))
+        return recm
+    if metrica == 'RECALL':
+        true_positive = 0
+        false_negative = 0
+        for i,j in zip(response[response.columns[0]],solution[solution.columns[0]]):
+            if (i == j) and (j == 0):
+                true_positive += 1
+            elif (i != j) and (j==0):
+                false_negative += 1
+        recall = true_positive/(true_positive+false_negative)
+        return recall
 
-use_metric = 'RECM'
+use_metric = 'RECALL'
 path_res = 'G:/My Drive/Datathon_responses/responses/'
 file_sol = 'G:/My Drive/Datathon_solutions/solution.csv'
 
 
 
 solution = pd.read_csv(file_sol)
-students = pd.read_excel('Data 02 Alumnos.xlsx')
+#students = pd.read_excel('Data 02 Alumnos.xlsx')
 
 
 while True:
@@ -47,7 +57,7 @@ while True:
 
     metric.columns = [use_metric]
 
-    metric.sort_values(by = use_metric, axis = 0, inplace=True)
+    metric.sort_values(by = use_metric, ascending= False, axis = 0, inplace=True)
 
     metric.to_excel('G:/My Drive/Datathon_solutions/metrics.xlsx')
     sleep(60)
