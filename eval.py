cd47cdbed6ad45959ef0c3ef63d5cd771f3bc6fe
J A Deiloff <jdeiloff@gmail.com>
2023-01-19 14:02:01 -0300

    data06vmtest branch added

--- eval.py
+++ eval.py
@@ -1,8 +1,37 @@
+import os
+base_path = r"C:\Users\viejo\Anaconda3"
+path = os.pathsep.join([os.path.join(base_path, i) for i in [r"", r"bin", r"Scripts", r"Library\mingw-w64\bin", r"Library\bin"]])
+os.environ["PATH"]+=os.pathsep+path
 from math import sqrt
 from pickletools import read_decimalnl_long
 import pandas as pd
-import os
 from time import sleep
+from oauth2client.service_account import ServiceAccountCredentials
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import gspread_dataframe as gd
+import gspread as gs
+from df2gspread import df2gspread as d2g
+import gspread
+from df2gspread import df2gspread as d2g
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'credential.json', scope)
+gc = gspread.authorize(credentials)
+
+'''
+gauth = GoogleAuth()
+
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', SCOPES)
+drive = GoogleDrive(gauth)
+query = "'{}' in parents and trashed=false"
+query=query.format(folder_id)
+file_list = drive.ListFile({'q': query}).GetList()
+'''
+
+
 
 def metrica(solution, response, metrica):
     if metrica == 'ECM':
@@ -33,43 +62,43 @@ def metrica(solution, response, metrica):
             return accuracy
 
 use_metric = ['RECALL', 'ACCURACY']
-path_res = 'G:/My Drive/Datathon_responses/responses/'
-file_sol = 'G:/My Drive/Datathon_solutions/solution.csv'
-
+path_res = "G:/.shortcut-targets-by-id/1L5as_lChFoOSJ5jGRPmbNyBx8OEVaa8ULNbodWk2VX9wJL7ibf0KZKre0sib84zLZuJmxg81/Datathon  (File responses)/Predicciones (File responses)/"
+file_sol = 'G:\Mi unidad\Datathon\solution\solution.csv'
 
 
 solution = pd.read_csv(file_sol)
 #students = pd.read_excel('Data 02 Alumnos.xlsx')
 
+files = os.listdir(path_res)
+responses = {}
+spreadsheet_key = '1732AeZw4d720B5tcm7bxn3YIgVITE6SbBfWiGzD9jB0'
 
 while True:
-    files = os.listdir(path_res)
-    responses = {}
     for file in files:
-        if '.csv' not in file:
-            print('Not in file')
-            continue
-        name = file.split(' ')
-        try:
-            response = pd.read_csv(path_res + file)
-        except:
-            print('cant read')
-            continue
-        try:
-            recm = metrica(solution, response, use_metric[0])
-            accuracy = metrica(solution, response, use_metric[1])
-            if name[0] in list(responses.keys()):
-                   responses[name[0]+name[len(name)-1].replace('.csv','')] = [recm,accuracy]
-            else:
-                responses[name[0]] = [recm,accuracy]
-        except:
-            print("cant save")
-            continue
+        if '.csv' in file:
+            name = file.split(' ')
+            try:
+                response = pd.read_csv(path_res + file)
+            except:
+                print('cant read')
+                continue
+            try:
+                recm = metrica(solution, response, use_metric[0])
+                accuracy = metrica(solution, response, use_metric[1])
+                if name[0] in list(responses.keys()):
+                        responses[name[0]+name[len(name)-1].replace('.csv','')] = [recm,accuracy]
+                else:
+                    responses[name[0]] = [recm,accuracy]
+            except:
+                print("cant save")
+                continue
     metric = pd.DataFrame(responses.values(), index = responses.keys())
 
     metric.columns = use_metric
 
     metric.sort_values(by = use_metric, ascending= False, axis = 0, inplace=True)
-
-    metric.to_excel('G:/My Drive/Datathon_solutions/metrics.xlsx')
-    sleep(60)
+    #metricsdirectory = "G:/.shortcut-targets-by-id/1L5as_lChFoOSJ5jGRPmbNyBx8OEVaa8ULNbodWk2VX9wJL7ibf0KZKre0sib84zLZuJmxg81/Datathon  (File responses)/metricas.gsheet"
+    #metric.to_excel(metricsdirectory)
+    #metric.to_csv('G:/.shortcut-targets-by-id/1L5as_lChFoOSJ5jGRPmbNyBx8OEVaa8ULNbodWk2VX9wJL7ibf0KZKre0sib84zLZuJmxg81/Datathon  (File responses)/metricas.csv')
+    d2g.upload(metric, spreadsheet_key, credentials=credentials, wks_name='Sheet1')
+    sleep(90)
